9e16a0f0b23c932582d48b0342bec45ec44cb0c3
Emiliano <russo.emiliano97@gmail.com>
2022-10-06 17:31:17 -0300

    show version in settings screen

--- src/screens/Settings/Settings.tsx
+++ src/screens/Settings/Settings.tsx
@@ -1,15 +1,15 @@
 import { Button } from "antd";
 import { useState } from "react";
 import { ModalChangeUsername } from "./ModalChangeUsername";
+const package_json = require("../../../package.json");
 
 export const Settings: React.FC = () => {
   const [changeUsernameVisible, setChangeUsernameVisible] = useState(false);
 
   return (
-    <div
-      style={{ margin: "5px auto", display: "flex", flexDirection: "column" }}
-    >
+    <div style={{ margin: "5px auto", display: "flex", flexDirection: "column" }}>
       <h1>Settings</h1>
+      <p style={{ textAlign: "center" }}> Jiren Games v{package_json.version}</p>
       <Button
         type="primary"
         style={{ margin: "5px" }}
